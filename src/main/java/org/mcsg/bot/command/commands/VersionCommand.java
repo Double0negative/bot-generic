d648401f551ddb9bce8d4833125caabdf4cb55c9
Double0negative <drew@superleague.com>
2017-02-28 16:09:00 -0800

    Add verison command

---
+++ src/main/java/org/mcsg/bot/command/commands/VersionCommand.java
@@ -0,0 +1,39 @@
+package org.mcsg.bot.command.commands;
+
+import org.mcsg.bot.api.Bot;
+import org.mcsg.bot.api.BotChat;
+import org.mcsg.bot.api.BotCommand;
+import org.mcsg.bot.api.BotUser;
+
+public class VersionCommand implements BotCommand {
+
+	@Override
+	public void execute(String cmd, BotChat chat, BotUser user, String[] args) throws Exception {
+		chat.sendMessage(chat.getServer().getBot().getBrandingString());
+	}
+
+	@Override
+	public String[] getPrefix() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getCommand() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHelp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
